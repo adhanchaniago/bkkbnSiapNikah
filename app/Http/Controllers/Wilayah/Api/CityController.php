64e4b93cffc9629befe1c000c8000c52d8b181ff
Adam Firdaus <adam.firdauz22@gmail.com>
2019-06-27 08:05:54 +0700

    add city model

--- app/Http/Controllers/Wilayah/Api/CityController.php
+++ app/Http/Controllers/Wilayah/Api/CityController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Wilayah\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravolt\Indonesia\Indonesia;
+use App\City;
 
 class CityController extends Controller
 {
     public function GetCities(){
-        $cities = Indonesia::allCities();
+        $cities = City::all();
         return response()->json([
             "message"=>"Berhasil mendapatkan data wilayah",
             "status"=> 200,
